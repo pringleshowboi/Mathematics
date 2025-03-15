8b49891acd9b7f9c14d117407fa9b747edace9d6
pringleshowboi <136586045+pringleshowboi@users.noreply.github.com>
2025-03-15 17:07:38 +0200

    Add files via upload
    MATH FIRST UPLOAD

---
+++ Algorithms.py
@@ -0,0 +1,51 @@
+'''
+https://www.youtube.com/watch?v=8hly31xKli0
+
+Algorithms:
+
+All computers follow a set of instructions creating algorithms
+What we focus on should be breaking down distinct components that satisfy our algorithm
+
+Tools and concepts to be made aware of:
+(We also need to think abstractly)
+Example
+
+A Game: Which one is correct?
+Person A: (Thinking of a number between 1-10)
+
+Person X: Does the range begin 1-10? (Establishing the contraints and boundaries) Guesses
+***Algorithmic thinking; defining problemset and define contraints and suitable insights***
+
+Person Y: Guesses
+----
+
+What solution works best for the problem?
+Algorithmic thinking involves trying to take the least amount of 'steps' to solve our problem, trying to solve our problem
+involves understanding our range, systematical thinking aswell as creative thinking on how massive our problem is.
+Algorithm; A set of instructions to reach our end goal with comparing our logic towards producing our solution.
+(Tries / Checksums == Solution)?
+
+There are many types of algorithms but these are what most algorithms are basically looking for:
+-----------------------------------------------------------------------------------------------------------------------------|
+Sorting algorithms: Arranging elements of a list or array in a specific order                                                |
+                                                                                                                             |
+Searching algorithms: Finding specific element or group of elements within a data structure                                  |
+                                                                                                                             |
+Dynamic programing: Solving problems by breaking them down into smaller overlapping subproblems and storing the solution to  |
+                    these subproblems and storing solutions of subproblems to avoid recomputation                            |
+                                                                                                                             |
+Greedy algorithms: Make locally optimal choices at each step with the hope of finding a global optimum                       |
+                                                                                                                             |
+Backtracking algorithms: Explore all possible solutions systematically trying different options and backtracking when dead   |
+                        end is reached                                                                                       |
+                                                                                                                             |                                                   
+Divide and Conquer algorithms: Solve a problem by breaking it down into smaller subproblems, solving each subproblem         |
+                        independently, and then combining the solutions to solve the original problem                        |
+                                                                                                                             |
+Machine learning algorithms: Enable computers to learn from data (an algorithm that learns from previous algorithms) without |
+                        human dependence                                                                                     |
+---------------------------------------------------------------------------------------------------------------------------- |
+
+
+
+'''
